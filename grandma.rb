4ce775ddef4cd6c299ffe1057e82ee14684653b3
karen ercoli <karenercoli@gmail.com>
2018-11-18 15:05:33 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 def speak_to_grandma(string)
-
   if string.upcase
     return "NO, NOT SINCE 1938!"
-  elsif string = 'I LOVE YOU GRANDMA!'
+  end
+  if string = 'I LOVE YOU GRANDMA!'
       return 'I LOVE YOU TOO PUMPKIN!'
     else
     return "HUH?! SPEAK UP, SONNY!"

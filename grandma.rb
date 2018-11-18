39eac1dba23fec4a654311a0da70b1844a6c6634
karen ercoli <karenercoli@gmail.com>
2018-11-18 15:11:07 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,6 @@
 def speak_to_grandma(string)
-  if string.upcase
-    return "NO, NOT SINCE 1938!"
-else
-      return "HUH?! SPEAK UP, SONNY!"
-end
+  if !string.upcase
+    return "HUH?! SPEAK UP, SONNY!"
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
